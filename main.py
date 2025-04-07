dcf97bc236e152fe79b8d3a3909ff222fe7cdd98
Sophia Williams <sophiamjwilliams@gmail.com>
2025-04-07 12:21:42 +0100

    changes
    my npz file was corrupted due to version changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-data = np.load("star_battle_dataset.npz", allow_pickle=True)
+data = np.load("star_battle_dataset_copy.npz", allow_pickle=True)
 puzzles = data["puzzles"]
 solutions = data["solutions"]
